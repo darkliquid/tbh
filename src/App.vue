915ec02fa2a103e91f9bc58be714578a86cafc3d
Andrew Montgomery <andrewm@streamelements.com>
2022-04-23 13:36:21 +0100

    feat: add dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
-  <v-app full-height>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+  <v-theme-provider :theme="darkMode ? 'dark' : 'light'">
+    <v-app full-height>
+      <v-app-bar app>
+        <v-app-bar-title>TBH</v-app-bar-title>
+        <v-spacer></v-spacer>
+        <v-switch
+          v-model="darkMode"
+          color="primary"
+          hide-details
+          density="compact"
+          class="dark-mode-switch"
+          :prepend-icon="darkMode ? 'mdi-weather-night' : 'mdi-white-balance-sunny'"
+        ></v-switch>
+      </v-app-bar>
+      <v-main>
+        <router-view />
+      </v-main>
+    </v-app>
+  </v-theme-provider>
 </template>
 
 <script setup>
-
+import { inject, ref } from 'vue'
+const initialDarkMode = inject('mq')
+const darkMode = ref(initialDarkMode.isDark)
 </script>
 
 <style>
-html, body, #app, #app .v-application {
-  /*min-height: 100%;*/
-}
-
 body {
   background-color:#efb71a;
   background-image: linear-gradient(30deg, #019fc7 12%, transparent 12.5%, transparent 87%, #019fc7 87.5%, #019fc7),
@@ -34,5 +46,9 @@ body {
 
 #app .v-btn--disabled.v-btn--disabled {
   color: rgba(var(--v-theme-on-surface), 0.26) !important;
+}
+
+.dark-mode-switch {
+  flex: 0 1 auto !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import { createPinia } from 'pinia'
 import router from './router'
+import { Vue3Mq } from "vue3-mq";
 
 loadFonts()
 
 createApp(App).use(createPinia())
   .use(vuetify)
   .use(router)
+  .use(Vue3Mq, { preset: 'vuetify'})
   .mount('#app')
